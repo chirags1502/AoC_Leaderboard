aec5c16905b6bb94d9aa0b28c69e1c072f424e22
Chirag Singhvi <chirags1502@gmail.com>
2020-12-03 23:03:16 -0500

    Added Player ID

--- leaderboard.py
+++ leaderboard.py
@@ -27,14 +27,15 @@ async def on_message(message):
     if message.content.startswith('$leaderboard'):
         try:
             response = requests.request("GET",url,cookies=cookie).json()
-            message_to_client= PrettyTable(['Name', 'Stars', 'Global Score', 'Local Score'])
+            message_to_client= PrettyTable(['ID','Name', 'Stars', 'Global Score', 'Local Score'])
 
             for key in response['members']:
                 name = response['members'][key]['name']
                 stars= response['members'][key]['stars']
                 local_score = response['members'][key]['local_score']
                 global_score = response['members'][key]['global_score']
-                message_to_client.add_row([name,stars, global_score, local_score])
+                player_id = response['members'][key]['id']
+                message_to_client.add_row([player_id,name,stars, global_score, local_score])
             await message.channel.send(message_to_client.get_string(sortby="Stars",reversesort=True))
         except:
             await message.channel.send("Could not retreive the leaderboard")
