31946332fbc90a4b6e63e6218b3b69d06d2ccc49
Chirag Singhvi <chirags1502@gmail.com>
2020-12-06 22:12:20 -0500

    Added last star timestamp

--- leaderboard.py
+++ leaderboard.py
@@ -2,16 +2,20 @@ import discord
 import requests
 import os
 from prettytable import PrettyTable
+from datetime import datetime as dt
+from pytz import timezone as tz
 
 API_KEY = os.getenv('API_KEY')
-cookie={'session': os.getenv('SESSION_COOKIE')}
+cookie = {'session': os.getenv('SESSION_COOKIE')}
 url = os.getenv('LEADERBOARD_API_URL')
 
 client = discord.Client()
 
+
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
+
 
 @client.event
 async def on_message(message):
@@ -20,19 +24,24 @@ async def on_message(message):
 
     if message.content.startswith('$leaderboard'):
         try:
-            response = requests.request("GET",url,cookies=cookie).json()
-            message_to_client= PrettyTable(['ID','Name', 'Stars', 'Global Score', 'Local Score'])
+            response = requests.request("GET", url, cookies=cookie).json()
+            message_to_client = PrettyTable(
+                ['ID', 'Name', 'Stars', 'Global Score', 'Local Score', 'Last Star Timestamp'])
 
             for key in response['members']:
                 name = response['members'][key]['name']
-                stars= response['members'][key]['stars']
+                stars = response['members'][key]['stars']
                 local_score = response['members'][key]['local_score']
                 global_score = response['members'][key]['global_score']
                 player_id = response['members'][key]['id']
-                message_to_client.add_row([player_id,name,stars, global_score, local_score])
-            await message.channel.send("```"+message_to_client.get_string(sortby="Stars",reversesort=True)+"```")
-        except:
+                last_star_ts = dt.fromtimestamp(int(response['members'][key]['last_star_ts']), tz(
+                    'US/Eastern')).strftime("%a, %b %d %Y at %H:%M:%S EST")
+                message_to_client.add_row(
+                    [player_id, name, stars, global_score, local_score, last_star_ts])
+            await message.channel.send("```"+message_to_client.get_string(sortby="Stars", reversesort=True)+"```")
+        except Exception as e:
+            print(e)
             await message.channel.send("Could not retreive the leaderboard")
-        
-        
+
+
 client.run(API_KEY)
